33c7d64d21a1f7c598fe4b8d62d21307396e268e
saladsisar <caesarg965@gmail.com>
2026-04-05 23:27:12 +0800

    Update graphtraversal.c

--- graphtraversal.c
+++ graphtraversal.c
@@ -9,5 +9,34 @@
 #include "main.c"
 
 // add code here for both 'get friend list' and 'get connection' here
+void getFriendList(struct Graph* graph, int id) {
+    if (id < 0 || id >= graph->numVertices) {
+        printf("Error: ID %d does not exist in the dataset.\n", id);
+        return;
+    }
 
+    struct Node* temp = graph->adjLists[id];
+    int count = 0;
+
+    // Count friends first
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("Person %d has %d friend(s)!\n", id, count);
+
+    if (count == 0) {
+        printf("List of friends: (none)\n");
+        return;
+    }
+
+    printf("List of friends:");
+    temp = graph->adjLists[id];
+    while (temp != NULL) {
+        printf(" %d", temp->id);
+        temp = temp->next;
+    }
+    printf("\n");
+}
 #endif
